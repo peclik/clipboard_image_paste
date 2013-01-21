1ea305eb87ff8b56088e4eabef7ad76f3b2df580
Richard Pecl <peclr@pouzitelne.cz>
2013-01-21 16:45:07 +0100

    added support for Redmine >=1.4.0

--- init.rb
+++ init.rb
@@ -9,18 +9,31 @@
 #*******************************************************************************
 
 require 'redmine'
-
-# Hooks
-require_dependency 'clipboard_image_paste/hooks'
-# Patches
-require_dependency 'clipboard_image_paste/attachment_patch'
+require 'dispatcher' unless Rails::VERSION::MAJOR >= 3
 
 Redmine::Plugin.register :clipboard_image_paste do
   name 'Clipboard image paste'
   author 'Richard Pecl'
   description "Paste cropped image from clipboard as attachment"
-  version '1.0'
+  version '1.1'
+  requires_redmine :version_or_higher => '1.4.0'
 
   configfile = File.join(File.dirname(__FILE__), 'config', 'settings.yml')
   $clipboard_image_paste_config = YAML::load_file(configfile)
+
+  redmineVer = Redmine::VERSION.to_a
+  $clipboard_image_paste_has_jquery = redmineVer[0] > 2 || (redmineVer[0] == 2 && redmineVer[1] >= 2)
+end
+
+
+if Rails::VERSION::MAJOR >= 3
+  ActionDispatch::Callbacks.to_prepare do
+    require_dependency 'clipboard_image_paste/hooks'
+    require_dependency 'clipboard_image_paste/attachment_patch'
+  end
+else
+  Dispatcher.to_prepare :clipboard_image_paste do
+    require_dependency 'clipboard_image_paste/hooks'
+    require_dependency 'clipboard_image_paste/attachment_patch'
+  end
 end
