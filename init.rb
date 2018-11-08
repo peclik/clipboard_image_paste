3c03ef3889f6836afcfa9340b2e387ead9560b59
Richard Pecl <peclr@pouzitelne.cz>
2018-11-08 19:36:50 +0100

    releasing v 1.13

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@
 # clipboard_image_paste Redmine plugin.
 #
 # Authors:
-# - Richard Pecl
+# - Richard Pecl & others (see README)
 #
 # Terms of use:
 # - GNU GENERAL PUBLIC LICENSE Version 2
@@ -16,7 +16,7 @@ Redmine::Plugin.register :clipboard_image_paste do
   author      'Richard Pecl'
   description 'Paste cropped image from clipboard as attachment'
   url         'http://www.redmine.org/plugins/clipboard_image_paste'
-  version     '1.12'
+  version     '1.13'
   requires_redmine :version_or_higher => '1.4.0'
 
   configfile = File.join(File.dirname(__FILE__), 'config', 'settings.yml')
